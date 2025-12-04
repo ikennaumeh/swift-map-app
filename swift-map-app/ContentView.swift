77b4ee06ec5114bfb4a0a8978f5ce8d1b14df8c7
ikenna <kennaumeh@gmail.com>
2025-12-04 10:59:12 +0100

    Initial Commit

---
+++ swift-map-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swift-map-app
+//
+//  Created by Ikenna Umeh on 04/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift-map-app/swift_map_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_map_appApp.swift
+//  swift-map-app
+//
+//  Created by Ikenna Umeh on 04/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct swift_map_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
