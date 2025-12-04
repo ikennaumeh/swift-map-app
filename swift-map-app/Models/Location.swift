08fb70eb072a4353d65d99f900015bbbd5691554
ikenna <kennaumeh@gmail.com>
2025-12-04 11:22:21 +0100

    Setup data model and architecture

---
+++ swift-map-app/Models/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  swift-map-app
+//
+//  Created by Ikenna Umeh on 04/12/2025.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name : String
+    let cityName : String
+    let coordinates : CLLocationCoordinate2D
+    let description : String
+    let imageNames : [String]
+    let link : String
+}
