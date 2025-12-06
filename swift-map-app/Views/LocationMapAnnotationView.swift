fb5cf2706528566a8ee9b2e89d3b17b33e54b599
ikenna <kennaumeh@gmail.com>
2025-12-06 23:34:47 +0100

    Added map pins

---
+++ swift-map-app/Views/LocationMapAnnotationView.swift
@@ -0,0 +1,43 @@
+//
+//  LocationMapAnnotationView.swift
+//  swift-map-app
+//
+//  Created by Ikenna Umeh on 06/12/2025.
+//
+
+import SwiftUI
+
+struct LocationMapAnnotationView: View {
+    
+    let accentColor: Color = Color("AccentColor")
+    
+    var body: some View {
+        VStack(spacing: 0){
+            Image(systemName: "map.circle.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 30, height: 30)
+                .font(.headline)
+                .foregroundStyle(.white)
+                .padding(6)
+                .background(accentColor)
+                .clipShape(RoundedRectangle(cornerRadius: 36))
+            
+            Image(systemName: "triangle.fill")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(accentColor)
+                .frame(width: 10, height: 10)
+                .rotationEffect(Angle(degrees: 180))
+                .offset(y: -3)
+                .padding(.bottom, 40)
+        }
+    }
+}
+
+#Preview {
+    ZStack {
+        Color.black.ignoresSafeArea()
+        LocationMapAnnotationView()
+    }
+}
--- swift-map-app/Views/LocationsView.swift
+++ swift-map-app/Views/LocationsView.swift
@@ -14,27 +14,14 @@ struct LocationsView: View {
     
     var body: some View {
         ZStack{
-            Map(position: $vm.cameraPosition)
+            mapLayer
+                .ignoresSafeArea()
             
             VStack(spacing: 10){
-                
                 header
                     .padding()
-                
                 Spacer()
-                
-                ZStack{
-                    ForEach(vm.locations) { location  in
-                        if(vm.mapLocation == location){
-                            LocationPreviewView(location: location)
-                                .shadow(color: .black.opacity(0.3), radius: 20)
-                                .padding()
-                                .transition(.asymmetric(
-                                    insertion: .move(edge: .trailing),
-                                    removal: .move(edge: .leading)))
-                        }
-                    }
-                }
+                locationsPreviewStack
             }
         }
     }
@@ -75,5 +62,38 @@ extension LocationsView {
         .background(.thickMaterial)
         .clipShape(RoundedRectangle(cornerRadius: 10))
         .shadow(color: .black.opacity(0.3), radius: 20, x: 0, y: 15)
+    }
+    
+    private var mapLayer: some View {
+        Map(position: $vm.cameraPosition){
+            ForEach(vm.locations) { location in
+                Annotation(
+                    location.name,
+                    coordinate: location.coordinates,
+                    anchor: .bottom){
+                        LocationMapAnnotationView()
+                            .scaleEffect(vm.mapLocation == location ? 1 : 0.7)
+                            .shadow(radius: 10)
+                            .onTapGesture {
+                                vm.showNextLocation(location: location)
+                            }
+                    }
+            }
+        }
+    }
+    
+    private var locationsPreviewStack: some View {
+        ZStack{
+            ForEach(vm.locations) { location  in
+                if(vm.mapLocation == location){
+                    LocationPreviewView(location: location)
+                        .shadow(color: .black.opacity(0.3), radius: 20)
+                        .padding()
+                        .transition(.asymmetric(
+                            insertion: .move(edge: .trailing),
+                            removal: .move(edge: .leading)))
+                }
+            }
+        }
     }
 }
